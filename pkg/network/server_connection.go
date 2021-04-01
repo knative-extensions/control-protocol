c948b15922c1f423cc4d397556ad3740361d8b81
Matthias Wessendorf <mwessend@redhat.com>
2021-04-01 12:01:08 +0200

    change arg name
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

--- pkg/network/server_connection.go
+++ pkg/network/server_connection.go
@@ -114,7 +114,7 @@ type serverTcpConnection struct {
 	tlsConfigLoader func() (*tls.Config, error)
 }
 
-func newServerTcpConnection(ctx context.Context, listener net.Listener, loader func() (*tls.Config, error)) *serverTcpConnection {
+func newServerTcpConnection(ctx context.Context, listener net.Listener, tlsConfigLoader func() (*tls.Config, error)) *serverTcpConnection {
 	c := &serverTcpConnection{
 		baseTcpConnection: baseTcpConnection{
 			ctx:                    ctx,
@@ -124,7 +124,7 @@ func newServerTcpConnection(ctx context.Context, listener net.Listener, loader f
 			errors:                 make(chan error, 10),
 		},
 		listener:        listener,
-		tlsConfigLoader: loader,
+		tlsConfigLoader: tlsConfigLoader,
 	}
 	return c
 }
