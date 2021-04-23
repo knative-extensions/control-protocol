49eac5a188190cc5c3f5a26e15aa218c6381ac37
slinkydeveloper <francescoguard@gmail.com>
2021-04-23 11:06:05 +0200

    Pod is either running or completed, but not failed
    Signed-off-by: Francesco Guardiani <francescoguard@gmail.com>

--- test/conformance/feature/conformance_feature.go
+++ test/conformance/feature/conformance_feature.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/stretchr/testify/require"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"knative.dev/control-protocol/test/conformance/resources/conformance_client"
 	kubeclient "knative.dev/pkg/client/injection/kube/client"
@@ -35,13 +36,17 @@ func ConformanceFeature(clientImage string, serverImage string) *feature.Feature
 		conformance_client.StartJob(client, clientImage, fmt.Sprintf("%s:%d", pod.Status.PodIP, port))(ctx, t)
 	})
 
-	f.Stable("Send and receive").Must("Pass", func(ctx context.Context, t feature.T) {
+	f.Stable("Send and receive").Must("Job should succeed", func(ctx context.Context, t feature.T) {
 		require.NoError(t, k8s.WaitUntilJobDone(
 			ctx,
 			kubeclient.Get(ctx),
 			environment.FromContext(ctx).Namespace(),
 			client,
 		))
+	}).Must("Pod shouldn't be failed", func(ctx context.Context, t feature.T) {
+		pod, err := kubeclient.Get(ctx).CoreV1().Pods(environment.FromContext(ctx).Namespace()).Get(ctx, server, metav1.GetOptions{})
+		require.NoError(t, err)
+		require.Contains(t, []corev1.PodPhase{corev1.PodRunning, corev1.PodSucceeded}, pod.Status.Phase)
 	})
 
 	return f
